5096f07ddc7441dd9959ca55ea3230f0ab57eccd
andersonwide <michaelmags42@gmail.com>
2021-10-12 20:10:13 -0300

    API-On-Azure

---
+++ StreamingSystem/Migrations/20211012222223_DBforAzure.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace StreamingSystem.Migrations
+{
+    public partial class DBforAzure : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
